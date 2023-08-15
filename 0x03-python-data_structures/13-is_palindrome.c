14a1a7815178835e1fb53a9133c0403198264b8b
Tai Keith <keithsteve.ks@hotmail.com>
2023-08-15 02:29:23 +0000

    A function that checks whether a linked list is a Palindrome or not

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,7 +11,9 @@
 int is_palindrome(listint_t **head)
 {
 	if (*head == NULL || (*head)->next == NULL)
+	{
 		return (1);
+	}
 
 	listint_t *slow = *head;
 	listint_t *fast = *head;
@@ -37,7 +39,9 @@ int is_palindrome(listint_t **head)
 	while (prev != NULL && slow != NULL)
 	{
 		if (prev->n != slow->n)
+		{
 			return (0);
+		}
 
 		prev = prev->next;
 		slow = slow->next;
