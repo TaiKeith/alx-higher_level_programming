063a11aad65273c7df52fd65aac9254da5ea08d5
Tai Keith <keithsteve.ks@hotmail.com>
2023-08-28 15:09:26 +0000

    A function that prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            printed += 1
+        except:
+            break
+    print()
+    return printed
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This directory contains Python based projects on Exceptions
