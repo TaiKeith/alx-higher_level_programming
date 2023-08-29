cae7c7e4bf4dd1b2f891e56711a0f723090b8621
Tai Keith <keithsteve.ks@hotmail.com>
2023-08-29 22:25:02 +0000

    A class Square with size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     and checks if size is an integer
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
