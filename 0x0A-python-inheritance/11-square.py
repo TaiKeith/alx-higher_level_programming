7371df76e3d81ad3dc749844cacc26f4d6343e30
Tai Keith <keithsteve.ks@hotmail.com>
2023-09-12 01:09:55 +0000

    Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Create a squate class that inherits 
+from the Rectangle class
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ create square instances """
+
+    def __init__(self, size):
+        """ instantiate size """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
