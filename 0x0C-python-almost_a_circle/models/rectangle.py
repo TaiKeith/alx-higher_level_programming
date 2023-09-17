2e030ce387ce5f80b5aae0e85cb389b73451a3ce
Tai Keith <keithsteve.ks@hotmail.com>
2023-09-17 09:22:05 -0700

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,3 +23,99 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """
+        Property method for width value
+
+        Return:
+            __width value
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        setter method for width value
+
+        Arg:
+            value (int): Value to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Property method for height value
+
+        Return:
+            __height value
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        setter method for height value
+
+        Arg:
+            value (int): Value to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Property method for x value
+        Return:
+            __x value
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        setter method for x value
+        Arg:
+            value (int): Value to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Property method for y value
+        Return:
+            __y value
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        setter method for y value
+        Arg:
+            value (int): Value to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
