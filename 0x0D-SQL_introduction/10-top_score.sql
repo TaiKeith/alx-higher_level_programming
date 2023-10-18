4fbb0dd6c787bc833c78e7fa9710ac02afda8b9a
Tai Keith <keithsteve.ks@hotmail.com>
2023-10-17 19:10:17 -0700

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- This script lists all records of second_table of a database
+-- Results display both score and te name
+-- Records should be ordered by best score
+SELECT score, name FROM second_table ORDER BY score DESC;
