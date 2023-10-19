2e0e53cef1ce5510b228736b8abf9eabd7d8e9fe
Tai Keith <keithsteve.ks@hotmail.com>
2023-10-19 14:12:47 -0700

    ID can't be null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- This script create a table id_not_null on MySQL server
+-- id attribute has a default value 1
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
