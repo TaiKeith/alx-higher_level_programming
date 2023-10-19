2ee7d726d5b96c10750db35ef558d14fc2471582
Tai Keith <keithsteve.ks@hotmail.com>
2023-10-19 15:59:09 -0700

    Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- This script lists all cities contained in database hbtn_0d_usa
+-- Each record displays cities.id - cities.name - states.name
+-- Results sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name FROM cities
+INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
