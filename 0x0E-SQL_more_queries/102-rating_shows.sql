eabdfaa6f55312c4f1c9240692f47ebb0ce5b1fa
Tai Keith <keithsteve.ks@hotmail.com>
2023-10-19 19:39:31 -0700

    Rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- This script lists all shows from hbtn_0d_tvshows_rate by their rating
+-- Each record displays: tv_shows.title - rating sum
+-- Results are sorted in descending order by the rating
+SELECT tv_shows.title, SUM(*) AS rating FROM tv_shows
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title ORDER BY rating DESC;
