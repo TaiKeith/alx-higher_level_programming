369572695fa6613fa4df83ffd689aaadeaf7fa80
Tai Keith <keithsteve.ks@hotmail.com>
2023-11-16 18:36:57 -0800

    Fixed indentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     Access the database and retrieves the states listed
     """
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], database=argv[3])
+                         passwd=argv[2], database=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     Access the database and retrieves all states starting with uppercase N
     """
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], database=argv[3])
+                         passwd=argv[2], database=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
             ORDER BY states.id ASC")
