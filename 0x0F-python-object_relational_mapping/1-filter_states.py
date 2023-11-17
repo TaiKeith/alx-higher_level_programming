afbee839e12eff866b694f358ad1c24101ac0b22
Tai Keith <keithsteve.ks@hotmail.com>
2023-11-17 11:45:37 -0800

    Fixed indentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], database=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
-            ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' "
+                "ORDER BY states.id ASC")
     result = cur.fetchall()
     for row in result:
         print(row)
