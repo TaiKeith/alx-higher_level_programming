9d2aba688d815ce1edb558c3dc4ead26f46a7b74
Tai Keith <keithsteve.ks@hotmail.com>
2023-11-17 14:27:38 -0800

    All cities by states

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,36 +5,27 @@ of that state from the database hbtn_0e_4_usa
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     """
     Connect to the MySQL server and retrieves all cities from the
     database
     """
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], database=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                                 passwd=sys.argv[2], database=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT cities.name, states.name FROM cities "
                 "JOIN states ON cities.state_id = states.id "
                 "WHERE states.name = %s "
-                "ORDER BY cities.id ASC", (argv[4],))
+                "ORDER BY cities.id ASC", (sys.argv[4],))
 
-    city_dict = {}
-    result = cur.fetchall()
-    for row in result:
-        city_name, state_name = row
-        if state_name not in city_dict:
-            city_dict[state_name] = set()
-        city_dict[state_name].add(city_name)
-
-    state_name = argv[4]
-    if state_name in city_dict:
-        cities = city_dict[state_name]
-        print(", ".join(cities))
-    else:
-        pass
+    cities = []
+    results = cur.fetchall()
+    for row in results:
+        cities.append(row[0])
+    print(", ".join(cities))
 
     cur.close()
     db.close()
