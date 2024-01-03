2245cd361b8c89302ad9da471f2a200e1e9b9e79
Tai Keith <keithsteve.ks@hotmail.com>
2024-01-03 05:56:38 -0800

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -10,7 +10,7 @@ const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
 	if (err) {
-		console.error(`${err.message}`);
+		console.error(err);
 	} else {
 		console.log(`${data}`);
 	}
