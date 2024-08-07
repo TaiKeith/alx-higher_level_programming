4beed801e6b99f7c5d96c9411dde5f173e9a57f7
Tai Keith <keithsteve.ks@hotmail.com>
2024-08-07 10:49:14 -0700

    Toggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    const toggleDiv = $('DIV#toggle_header');
+    const headerElement = $('header');
+
+    toggleDiv.click(function() {
+        headerElement.toggleClass('red green');
+    });
+});
