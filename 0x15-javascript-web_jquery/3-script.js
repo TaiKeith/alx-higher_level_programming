1f67f92295d39f6f4ad4d06fb1049cc164ba12df
Tai Keith <keithsteve.ks@hotmail.com>
2024-08-07 10:37:41 -0700

    Add '.red' class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    const redHeaderDiv = $('DIV#red_header');
+
+    redHeaderDiv.click(function() {
+        const headerElement = $('header');
+
+        if (headerElement) {
+            headerElement.addClass('red');
+        } else {
+            console.error('Header elemnet not found');
+        }
+    });
+});
