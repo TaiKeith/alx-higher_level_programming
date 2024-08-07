331914a0bca37d26c2ee5b2d54c6390e056ffdd5
Tai Keith <keithsteve.ks@hotmail.com>
2024-08-07 10:24:17 -0700

    With JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+        const element = $('header');
+        if (element) {
+                element.css('color', '#FF0000');
+        } else {
+                console.error('HTML tag not found');
+        }
+});
